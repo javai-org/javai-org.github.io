cb23cb459db38ca44276388c5be3cdd483661cf6
Mike Mannion <mike.mannion@karakun.com>
2026-03-27 22:56:14 +0100

    Overdue commit and push.

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+rootProject.name = "javai"
+
+val newsroomDir = file("../javai-newsroom")
+if (newsroomDir.isDirectory) {
+    includeBuild(newsroomDir) {
+        dependencySubstitution {
+            substitute(module("org.javai:javai-newsroom")).using(project(":"))
+        }
+    }
+}
+
+// Use local punit when available (sibling folder), Maven Central otherwise
+val punitDir = file("../punit")
+if (punitDir.isDirectory) {
+    includeBuild(punitDir) {
+        dependencySubstitution {
+            substitute(module("org.javai:punit-core")).using(project(":punit-core"))
+            substitute(module("org.javai:punit-junit5")).using(project(":punit-junit5"))
+        }
+    }
+}
+
+// Use local outcome when available (sibling folder), Maven Central otherwise
+val outcomeDir = file("../outcome")
+if (outcomeDir.isDirectory) {
+    includeBuild(outcomeDir) {
+        dependencySubstitution {
+            substitute(module("org.javai:outcome")).using(project(":"))
+        }
+    }
+}
